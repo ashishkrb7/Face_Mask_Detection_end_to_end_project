caea89b5af1633e3671cec9d1a251a22e3cdb645
Ashish Kumar <60195105+ashishcssom@users.noreply.github.com>
2020-08-24 02:55:13 +0530

    Update Engine.py

--- Engine.py
+++ Engine.py
@@ -1,7 +1,6 @@
 import warnings
 warnings.filterwarnings("ignore")
 import cv2
-import imutils
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
