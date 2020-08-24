e2efa460ccf2fbb8f2f6cc579f4d88c745281678
Ashish Kumar <60195105+ashishcssom@users.noreply.github.com>
2020-08-25 01:10:56 +0530

    Update MainAPI.bat

--- MainAPI.bat
+++ MainAPI.bat
@@ -3,7 +3,7 @@ SET LOGFILE="%~dp0\log\MainAPIlog.log"
 (echo====================================================================================================== >> %LOGFILE%)
 (echo Script Start Running at - ^ %date% %time% >> %LOGFILE%)
 call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
-"C:\Users\css120804\.conda\envs\FaceDetection\python.exe" "%~dp0\app.py"
+"C:\Users\imash\.conda\envs\FaceDetection\python.exe" "%~dp0\app.py"
 (echo Script Successfully Executed at - ^ %date% %time% >> %LOGFILE%)
 (echo====================================================================================================== >> %LOGFILE%)
 pause
