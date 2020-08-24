616171ded25e699b85e6339b9a5c3161d40baa8d
Ashish Kumar <60195105+ashishcssom@users.noreply.github.com>
2020-08-25 01:22:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 prototxtPath="./models/deploy.prototxt" # Face detector model
-weightsPath="./models/res10_300x300_ssd_iter_140000.caffemodel" # Trained face mask detector model
+weightsPath="./models/res10_300x300_ssd_iter_140000.caffemodel" # Face detector model weights
 confidence=0.5
-model="./models/model"
+model="./models/model" # Trained face mask detector model
 UPLOAD_FOLDER='./static/uploads/'
 DOWNLOAD_FOLDER='./static/downloads/'
